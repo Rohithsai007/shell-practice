1c8d81efebb40de7e71af57ce175fddb6d974e27
sai rohith <www.sai.daring.5009@gmail.com>
2025-09-22 16:57:36 +0530

    08 commit

--- 08-specialvar.sh
+++ 08-specialvar.sh
@@ -2,4 +2,10 @@
 
 echo "All varibles passed through script:$@"
 echo "All varibles passed through script:$*"
-echo "shellscript :$0"
+echo "shellscriptname :$0"
+echo "print the current working dir:$pwd"
+echo "who am i: $user"
+echo "print the HOME director:$home"
+echo"print the process id:$$'
+sleep  20 &
+echo "print pid of the last process in bg:$!"
