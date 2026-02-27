75c161cae40642dd36ea6e9f7e5295ae2a0b0825
sai rohith <www.sai.daring.5009@gmail.com>
2026-02-27 07:50:28 +0530

    var

---
+++ 02-varibles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#no spaces before and after
+person1=ramesh
+person2=suresh
+
+echo "$person1::hello bro how are you $person2"
+echo "$person2::great day bro $person1"
---
+++ 15-colors.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+R = \e[31m
+G = \e[32m
+Y = \e[33m
+N = \e[0m
+
+echo -e "$G Helloworld $N"
+echo -e "$B Hello world"
+echo -e "$Y Hello world"
