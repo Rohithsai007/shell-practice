078c5db29cb3b05e75c7f8efb88f319213033c06
sai rohith <www.sai.daring.5009@gmail.com>
2026-02-27 09:10:19 +0530

    firts

---
+++ dynamic-arg.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+person1=$1
+person2=$2
+echo"$person1::hello $person2"
+echo"$person2:: hi $person1"
+echo"$person1::welcome to russia $person2"
+echo"$person2:: good moring aleska $person1"
