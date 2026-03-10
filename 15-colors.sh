5088fea754191286dbd552f8665b0f3a556eb829
sai rohith <www.sai.daring.5009@gmail.com>
2026-03-10 11:20:31 +0530

    gsggs

--- 15-colors.sh
+++ 15-colors.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-R = "\e[31m"
-G = "\e[32m"
-Y = "\e[33m"
-N = "\e[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
 
-echo -e "$G Helloworld $N"
-echo -e "$B Hello world $N"
-echo -e "$Y Hello world $N"
-
-
-
-
+echo -e "${G} Hello world ${N}"
+echo -e "${B} Hello world ${N}"
+echo -e "${Y} Hello world ${N}"
