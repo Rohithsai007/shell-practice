15e2477a23a2f1f906ea78829dccffafc02be825
sai rohith <www.sai.daring.5009@gmail.com>
2026-03-10 11:13:23 +0530

    gsggss

--- 14-installtionfunc.sh
+++ 14-installtionfunc.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-USERID = $(id -u)
+USERID=$(id -u)
 
-if [ USERID -ne 0];  then
-    echo "$USERID::please enter root acces to eneter"
+if [ $USERID -ne 0 ]; then
+    echo "$USERID :: Please run with root access"
     exit 1
 fi
 
-VALIDATE(){ # functions receive inputs through args just like shell script args
-
-if [ $1 -ne 0]: then
-    echo "ERROR:: $2 installation is failure"
-else
-    echo "$2 installtion is complete"
-fi
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo "ERROR :: $2 installation failed"
+    else
+        echo "SUCCESS :: $2 installation completed"
+    fi
 }
 
 dnf install mysql -y
 VALIDATE $? "mysql"
 
-
-dnf install mysql -y
+dnf install nginx -y
 VALIDATE $? "nginx"
 
-
-dnf install mysql -y
+dnf install python3 -y
 VALIDATE $? "python3"
